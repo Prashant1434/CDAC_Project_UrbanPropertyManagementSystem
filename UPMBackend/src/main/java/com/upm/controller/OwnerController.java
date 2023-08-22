6bdc6a71d346ba2b146770ffc8d59ec80834d4b6
Nileshd2511 <nilesh.d2511@gmail.com>
2023-08-22 21:05:02 +0530

    added controller

--- UPMBackend/src/main/java/com/upm/controller/OwnerController.java
+++ UPMBackend/src/main/java/com/upm/controller/OwnerController.java
@@ -1,4 +1,5 @@
 package com.upm.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.upm.dto.AddTenantDto;
+import com.upm.dto.AddUtilityDto;
 import com.upm.service.OwnerService;
 
 @RestController
@@ -16,7 +18,7 @@ public class OwnerController {
 	private OwnerService ownerService;
 
 	public OwnerController() {
-		System.out.println("in ctor : "+getClass());
+		System.out.println("in ctor : " + getClass());
 	}
 
 	@PostMapping("/addTenant")
@@ -24,9 +26,15 @@ public class OwnerController {
 		System.out.println(tenantDto.toString());
 		return ownerService.addTenant(tenantDto);
 	}
-	
+
 	@PostMapping("/assignFlatToTenant/{id}/{tId}")
 	public String assignFlatToTenant(@PathVariable Long id, @PathVariable Long tId) {
 		return ownerService.assignFlatToTenant(id, tId);
+	}
+
+	@PostMapping("/assignUtilityToTenant/{fid}/{tid}")
+	public String assignUtilityToTenant(@PathVariable Long fid, @PathVariable Long tid,
+			@RequestBody AddUtilityDto addUtilityDto) {
+		return ownerService.assignUtilityToTenant(fid, tid, addUtilityDto);
 	}
 }
