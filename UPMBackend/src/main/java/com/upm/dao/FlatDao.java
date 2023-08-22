0cf5b3597886ff57bef69232fdc6e9bd6d72232c
Nileshd2511 <nilesh.d2511@gmail.com>
2023-08-22 21:15:39 +0530

    deleted

--- UPMBackend/src/main/java/com/upm/dao/FlatDao.java
+++
@@ -1,9 +0,0 @@
-package com.upm.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.upm.entities.Flat;
-
-public interface FlatDao extends JpaRepository<Flat, Long> {
-
-}
--- UPMBackend/src/main/java/com/upm/dao/RentDao.java
+++
@@ -1,9 +0,0 @@
-package com.upm.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.upm.entities.Rent;
-
-public interface RentDao extends JpaRepository<Rent, Long> {
-
-}
