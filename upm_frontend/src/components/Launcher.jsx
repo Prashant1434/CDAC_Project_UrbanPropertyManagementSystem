2de278ab768d8c3dd0b464ac8b8146212b20e2fc
appletrain <sakshamdeshmukh007@gmail.com>
2023-08-24 17:51:10 +0530

    added homepage

--- upm_frontend/src/components/Launcher.jsx
+++ upm_frontend/src/components/Launcher.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Homepage from "./Homepage";
+import Navbar from "./Navbar";
+
 function Launcher() {
-    return ( <><h1> Hi From Launcher</h1></> );
+    return ( <Router>
+        <Routes>
+            <Route exact path='/Homepage' Component={Homepage}/>
+            <Route exact path='/Navbar' Component={Navbar}/>
+        </Routes>
+    </Router>);
 }
 
 export default Launcher;
---
+++ upm_frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+    import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+    import '../css/Navbar.css'
+
+    function Navbar() {
+        return ( 
+           <>
+           <nav class="navbar navbar-default" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Urban Proerty Management</a>
+                
+            </div>
+
+           
+            <div class="collapse navbar-collapse navbar-ex1-collapse">                
+                <button type="button" class="btn btn-primary login" >Login</button>
+                
+            </div>
+           </nav>
+
+        
+     
+           </>
+         );
+    }
+    
+    export default Navbar;
