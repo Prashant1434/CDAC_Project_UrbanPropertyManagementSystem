66fee5cc67a5c7173a29808adf113cfe2eea059c
Nileshd2511 <nilesh.d2511@gmail.com>
2023-08-24 12:11:10 +0530

    added Tenant Service and Tenant Controller

--- UPMBackend/src/main/java/com/upm/controller/TenantController.java
+++ UPMBackend/src/main/java/com/upm/controller/TenantController.java
@@ -1,4 +1,33 @@
 package com.upm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.upm.entities.Tenant;
+import com.upm.service.TenantService;
+import com.upm.service.UserService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/tenant")
 public class TenantController {
+	
+	@Autowired
+	private TenantService tenantService;
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	
+	@PutMapping("/rentPayment/{id}")
+	public String rentPayment(@PathVariable Long id) {
+		return tenantService.updateRentStatus(false, id);
+	}
 }
