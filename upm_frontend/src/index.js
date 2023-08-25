c883eaa0d8e2c1fd4229e4eb034f36b42c6279ef
Nileshd2511 <nilesh.d2511@gmail.com>
2023-08-25 09:30:19 +0530

    frontend deleted

--- upm_frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Launcher from './components/Launcher';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Launcher/>
-);
